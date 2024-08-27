13fe82d202968fa87b75b4fd3889a79c2950ae67
Hoby <rabhob@yahoo.fr>
2024-08-27 20:47:45 +0200

    Provide a security login layer using Spring Boot

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/WebSecurityConfigurerAdapter.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/WebSecurityConfigurerAdapter.java
@@ -1,4 +1,4 @@
-package Configuration;
+package com.nnk.springboot.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
